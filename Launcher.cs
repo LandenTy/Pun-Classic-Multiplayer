7d2cc35427af3c1cfd075721dc9ecc77691bc7e8
LandenTy <93301002+LandenTy@users.noreply.github.com>
2022-02-04 16:24:28 -0600

    Update Launcher.cs

--- Launcher.cs
+++ Launcher.cs
@@ -6,16 +6,22 @@ using UnityEngine.UI;
 
 public class Launcher : MonoBehaviour {
 
-	[SerializeField] InputField roomNameInputField;
+	public static Launcher Instance;
+
 	[SerializeField] Text roomNameText;
 	[SerializeField] Text errorText;
+	[SerializeField] Text enteringText;
 	[SerializeField] Transform roomListContent;
 	[SerializeField] GameObject roomListItemPrefab;
+
+	public RoomListItem roomListItem;
 
 	private string _gameVersion = "v0.0.1";
 
 	void Awake()
 	{
+		Instance = this;
+
 		//Lets us use PhotonNetwork.LoadLevel() to properly connect/disconnect clients to the server
 		PhotonNetwork.automaticallySyncScene = true;
 	}
@@ -40,16 +46,11 @@ public class Launcher : MonoBehaviour {
 
 	public void CreateRoom()
 	{
-		if (string.IsNullOrEmpty (roomNameInputField.text)) 
-		{
-			return;
-		}
-
 		RoomOptions roomOptions = new RoomOptions();
 		roomOptions.IsVisible = true;
 		roomOptions.MaxPlayers = 6;
 
-		PhotonNetwork.CreateRoom (roomNameInputField.text);
+		PhotonNetwork.CreateRoom ("");
 		MenuManager.Instance.OpenMenu ("loading");
 	}
 
@@ -65,8 +66,29 @@ public class Launcher : MonoBehaviour {
 		MenuManager.Instance.OpenMenu ("error");
 	}
 
+	public void LeaveRoom()
+	{
+		PhotonNetwork.LeaveRoom ();
+		MenuManager.Instance.OpenMenu ("loading");
+	}
+
+	public void JoinRoom(RoomInfo info)
+	{
+		PhotonNetwork.JoinRoom (info.Name);
+		MenuManager.Instance.OpenMenu ("loading");
+	}
+
 	public void OnRoomListUpdate(List<RoomInfo>  roomList)
 	{
+		foreach (Transform trans in roomListContent) 
+		{
+			Destroy (trans.gameObject);
+		}
 
+		for (int i = 0; i < roomList.Count; i++) {
+			Instantiate (roomListItemPrefab);
+			Instantiate (roomListContent);
+			roomListItem.SetUp (roomList[i]);
+		}
 	}
 }
